a6b9f269061344c217c39e439fdec910e75313d2
Aly Ayman <alyayman921@gmail.com>
2024-10-06 21:23:47 +0300

    readme update

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Thrust-Tester-gui
+
+
+Python 3.12
+
+pip3 install tkinter
+pip3 install pyserial
+
+That's it
--- build.bat
+++
@@ -1,2 +0,0 @@
-pyinstaller --onefile --onedir --noconsole  --add-data=controller_assets:controller_assets --icon controller_assets/icon.ico GUI3.py
-pause
